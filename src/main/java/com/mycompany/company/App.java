0ca799520adb7c6eae948a4d7f4b769eb147debb
Sreedhar Guda <sreedharab@gmail.com>
2016-06-02 15:18:42 -0400

    updated good

--- src/main/java/com/mycompany/company/App.java
+++ src/main/java/com/mycompany/company/App.java
@@ -6,7 +6,7 @@ package com.mycompany.company;
  */
 public class App 
 {
-    pulic static void main( String[] args )
+    public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
